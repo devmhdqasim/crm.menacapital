3baaf09f37bcb444ae5502f8e333eae46ba935ba
Muhammad Qasim <dev.mhdqasim@gmail.com>
2025-12-16 01:28:01 +0500

    agent leads tweak

--- src/routes/Agents/LeadsListing/index.jsx
+++ src/routes/Agents/LeadsListing/index.jsx
@@ -158,11 +158,23 @@ const LeadsListing = ({
               }`}
             >
               <span>{tab}</span>
-              {agentLeadsCount?.[tab?.replace(/\s+/g, '')] ? (
-                <span className="inline-flex items-center justify-center px-2 py-0.5 text-xs font-bold leading-none text-white bg-red-500 rounded-full animate-pulse">
-                  {agentLeadsCount?.[tab?.replace(/\s+/g, '')]}
-                </span>
-              ): ''}
+              {tab?.replace(/\s+/g, '') == 'Pending' ? (
+                <>
+                  {agentLeadsCount?.['NotAssigned'] ? (
+                    <span className="inline-flex items-center justify-center px-2 py-0.5 text-xs font-bold leading-none text-white bg-red-500 rounded-full animate-pulse">
+                      {agentLeadsCount?.['NotAssigned']}
+                    </span>
+                  ): ''}
+                </>
+              ) : (
+                <>
+                  {agentLeadsCount?.[tab?.replace(/\s+/g, '')] ? (
+                    <span className="inline-flex items-center justify-center px-2 py-0.5 text-xs font-bold leading-none text-white bg-red-500 rounded-full animate-pulse">
+                      {agentLeadsCount?.[tab?.replace(/\s+/g, '')]}
+                    </span>
+                  ): ''}
+                </>
+              )}
             </button>
           ))}
         </div>
