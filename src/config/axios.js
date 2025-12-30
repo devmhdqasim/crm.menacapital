f8cb846f952da1f10483ad929745873e6b335d1a
devqasimgenetics <mobileapp@saveingold.ae>
2025-12-30 23:59:32 +0500

    latest staging changes

--- src/config/axios.js
+++ src/config/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'https://staging.crm.saveingold.app/api/v1';
+const baseURL = 'https://api.crm.saveingold.app/api/v1';
 
 const axiosInstance = axios.create({
   baseURL,
